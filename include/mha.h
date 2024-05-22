87e64e0ed0c679bdf22d2f35d3692c5d7bb23bce
a3a256 <81564718+a3a256@users.noreply.github.com>
2024-05-22 23:15:33 +0100

    added scaled-dot-product attention layer

--- include/mha.h
+++ include/mha.h
@@ -8,21 +8,6 @@
 #include <climits>
 #include <numeric>
 
-std::vector<std::vector<float>> attention_layer(std::vector<std::vector<float>> q,
-                                                std::vector<std::vector<float>> k,
-                                                std::vector<std::vector<float>> v,
-                                                int dk){
-    std::vector<std::vector<float>> first_mul;
-    first_mul = matmul(q, transpose(k));
-    float dk_sqrt = std::sqrt((float)dk);
-    int i, j;
-    for(i=0; i<first_mul.size(); i++){
-        for(j=0; j<first_mul[i].size(); j++){
-            first_mul[i][j] = first_mul[i][j]/dk_sqrt;
-        }
-    }
-}
-
 class MultiHeadAttention{
     public:
         int em_size;
@@ -51,5 +36,24 @@ class MultiHeadAttention{
             std::mt19937 rng(seeder());
             std::uniform_int_distribution<long> gen(INT_MIN, INT_MAX);
             return (float)gen(rng)/(float)RAND_MAX;
+        }
+
+        std::vector<std::vector<float>> attention_layer(std::vector<std::vector<float>> q,
+                                                std::vector<std::vector<float>> k,
+                                                std::vector<std::vector<float>> v,
+                                                int dk){
+            std::vector<std::vector<float>> first_mul;
+            first_mul = matmul(q, transpose(k));
+            float dk_sqrt = std::sqrt((float)dk);
+            int i, j;
+            for(i=0; i<first_mul.size(); i++){
+                for(j=0; j<first_mul[i].size(); j++){
+                    first_mul[i][j] = first_mul[i][j]/dk_sqrt;
+                }
+            }
+            first_mul = softmax(first_mul);
+            first_mul = matmul(first_mul, v);
+
+            return first_mul;
         }
 };
